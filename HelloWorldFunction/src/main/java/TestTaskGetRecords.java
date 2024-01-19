7c1fb15120deadd69f81e57e536d1c4a874d0d1b
Viktor Oliynyk <viktor.oliynyk@intellias.com>
2024-01-19 16:46:55 +0200

    Test task implementation

--- HelloWorldFunction/src/main/java/TestTaskGetRecords.java
+++ HelloWorldFunction/src/main/java/TestTaskGetRecords.java
@@ -3,10 +3,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import dto.TestTaskGetRequest;
 import dto.TestTaskGetResponse;
 import dto.TestTaskRecord;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -29,16 +26,16 @@ public class TestTaskGetRecords implements RequestHandler<TestTaskGetRequest, Te
 
   public QueryResponse getTestTaskRecords(TestTaskGetRequest getRequest) {
     Map<String, String> expressionAttributesNames = new HashMap<>();
-    expressionAttributesNames.put("#" + ORG_ID, ORG_ID);
+    expressionAttributesNames.put("#orgId", ORG_ID);
     Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
     expressionAttributeValues.put(
-        ":" + ORG_ID, AttributeValue.builder().s(getRequest.getOrgId()).build());
+        ":orgIdValue", AttributeValue.builder().s(getRequest.getOrgId()).build());
 
     QueryRequest request =
         QueryRequest.builder()
             .tableName(DYNAMODB_TABLE_NAME)
             .indexName(ORG_ID_INDEX_NAME)
-            .keyConditionExpression(String.format("#%s = :%s", ORG_ID, ORG_ID))
+            .keyConditionExpression("#orgId = :orgIdValue")
             .expressionAttributeNames(expressionAttributesNames)
             .expressionAttributeValues(expressionAttributeValues)
             .build();
@@ -55,6 +52,13 @@ public class TestTaskGetRecords implements RequestHandler<TestTaskGetRequest, Te
   }
 
   private TestTaskRecord buildRecord(Map<String, AttributeValue> map) {
-    return new TestTaskRecord(map.get("recordId").s(), map.get("name").s(), map.get(ORG_ID).s());
+    return new TestTaskRecord(
+        getAttributeValue(map.get("recordId")),
+        getAttributeValue(map.get("name")),
+        getAttributeValue(map.get(ORG_ID)));
+  }
+
+  private String getAttributeValue(AttributeValue attribute) {
+    return Optional.ofNullable(attribute).map(AttributeValue::s).orElse("");
   }
 }
